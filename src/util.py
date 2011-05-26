80df46cee7f3b53f534e44a8bbf1a9b19f7971fe
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-05-26 14:39:49 -0400

    Added KeyRemover (and tests) to util
    KeyRemover will be used in the Dict converter for filtering out
    keys that we don't want in the resulting conversion

--- src/util.py
+++ src/util.py
@@ -15,3 +15,27 @@ class StackCounter(object):
 
     def can_increase(self):
         return (self.stacklimit is None) or (self.stack < self.stacklimit)
+
+class KeyRemover(object):
+
+    def __init__( self, removekeys={} ):
+        self.removestack = [ removekeys ]
+
+    @property
+    def keys(self):
+        return self.removestack[-1]
+
+    def filter(self, all_keys):
+        return [ x for x in all_keys if x not in self.keys or self.keys[x] is not True ]
+
+    def enter_key(self, key):
+        context = self.keys.get(key, {})
+        if type(context) is not dict:
+            raise ValueError("remove context when entering key %s was not a dict" % key )
+        self.removestack.append( context )
+
+    def leave_key(self):
+        if len( self.removestack ) == 1:
+            raise IndexError("cannot remove last context from stack")
+        self.removestack.pop()
+
--- tests/test_util.py
+++ tests/test_util.py
@@ -67,6 +67,134 @@ class TestStackCounter( unittest.TestCase ):
             c.decrease()
             self.assertTrue( c.can_increase() )
 
+
+
+class TestKeyRemover( unittest.TestCase ):
+
+    def setUp(self):
+        self.remover = KeyRemover()
+
+    def test_filter_nothing(self):
+        self.assertEquals( self.remover.filter([]), [] )
+
+    def test_filter_no_remove(self):
+        k = ['a', 'b', 'c', 'd']
+        self.assertEquals( self.remover.filter( k ), k )
+
+    def test_stack_size_is_one(self):
+        self.assertEquals( len(self.remover.removestack), 1 )
+
+    def test_keys_are_empty(self):
+        self.assertEquals( len(self.remover.keys) , 0 )
+
+    def test_leave_key_raises_error(self):
+        self.assertRaises( IndexError, self.remover.leave_key )
+
+    def test_enter_unknown_key_returns_full_list(self):
+        k = ['a', 'b', 'c', 'd']
+        self.remover.enter_key('e')
+        self.assertEquals( self.remover.filter( k ), k )
+        self.remover.leave_key()
+        self.assertEquals( len( self.remover.removestack ), 1 )
+
+    def test_filter_one_key(self):
+        remover = KeyRemover({'a' : True})
+        keys = ['a', 'b', 'c', 'd']
+        result = ['b', 'c', 'd']
+        self.assertEquals( remover.filter( keys ), result )
+
+    def test_filter_two_keys(self):
+        remover = KeyRemover({'a' : True, 'b' : True})
+        keys = ['a', 'b', 'c', 'd']
+        result = ['c', 'd']
+        self.assertEquals( remover.filter( keys ), result )
+
+    def test_filter_nested_remove(self):
+        r = {'a' : True, 'b' : True, 'c' : {'d' : True} }
+        remover = KeyRemover( r )
+        keys = ['a', 'b', 'c', 'd']
+        result = ['c', 'd']
+        self.assertEquals( remover.filter( keys ), result )
+
+    def test_entering_remove_key(self):
+        k = {'a' : True, 'b' : {'c' : True} }
+        r = {'c' : True}
+
+        remover = KeyRemover( k )
+        remover.enter_key( 'b' )
+        self.assertEquals( remover.keys, r )
+        remover.leave_key()
+        self.assertEquals( remover.keys, k )
+
+    def test_enter_remove_key_filter(self):
+        remove = {'a' : True, 'b' : {'c' : True, 'd' : True} }
+        keys = ['a', 'b', 'c', 'd']
+
+        remover = KeyRemover( remove )
+        self.assertEquals( remover.filter( keys ), ['b', 'c', 'd'] )
+
+        remover.enter_key( 'b' )
+        self.assertEquals( remover.filter( keys ), ['a', 'b'] )
+        remover.leave_key()
+        self.assertEquals( remover.filter( keys ), ['b', 'c', 'd' ] )
+
+    def test_enter_multiple_remove_keys(self):
+        remove = {'a' : True, 'b' : {'c' : True, 'd' : {'e' : True, 'f' : True } } }
+
+        remover = KeyRemover( remove )
+        self.assertEquals( remover.keys, remove )
+        self.assertEquals( len( remover.removestack), 1 )
+
+        remover.enter_key( 'b' )
+        self.assertEquals( remover.keys, remove['b'] )
+        self.assertEquals( len( remover.removestack), 2 )
+
+        remover.enter_key( 'd' )
+        self.assertEquals( remover.keys, remove['b']['d'] )
+        self.assertEquals( len( remover.removestack), 3 )
+
+        remover.enter_key( 'g' )
+        self.assertEquals( remover.keys, {} )
+        self.assertEquals( len( remover.removestack), 4 )
+
+        remover.leave_key()
+        self.assertEquals( remover.keys, remove['b']['d'] )
+        self.assertEquals( len( remover.removestack), 3 )
+
+        remover.leave_key()
+        self.assertEquals( remover.keys, remove['b'] )
+        self.assertEquals( len( remover.removestack), 2 )
+
+        remover.leave_key()
+        self.assertEquals( remover.keys, remove )
+        self.assertEquals( len( remover.removestack), 1 )
+
+    def test_enter_multiple_remove_filter(self):
+        remove = {'a' : True, 'b' : {'c' : True, 'd' : {'e' : True, 'f' : True } } }
+        keys = ['a', 'b', 'c', 'd', 'e', 'f']
+
+        remover = KeyRemover( remove )
+        self.assertEquals( remover.filter( keys ), keys[1:] )
+
+        remover.enter_key( 'b' )
+        self.assertEquals( remover.filter( keys ), keys[0:2] + keys[3:] )
+
+        remover.enter_key( 'd' )
+        self.assertEquals( remover.filter( keys ), keys[:4] )
+
+        remover.enter_key( 'g' )
+        self.assertEquals( remover.filter( keys ), keys )
+
+        remover.leave_key()
+        self.assertEquals( remover.filter( keys ), keys[:4] )
+
+        remover.leave_key()
+        self.assertEquals( remover.filter( keys ), keys[0:2] + keys[3:] )
+
+        remover.leave_key()
+        self.assertEquals( remover.filter( keys ), keys[1:] )
+
+
 if __name__ == '__main__':
 
     import os.path
